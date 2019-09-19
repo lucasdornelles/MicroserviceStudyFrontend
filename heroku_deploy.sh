a094e0169b935cb0a6d4d84ff2b47add4b36d518
Lucas <lucasdornellesr@gmail.com>
2019-09-19 18:28:32 -0300

    lets try it the hard way, manualy uploading slugs to heroku using deploy_heroku.sh

---
+++ heroku_deploy.sh
@@ -0,0 +1,39 @@
+#!bin/bash
+echo "Preparing slug contents"
+mkdir app
+cp -r ./build ./app
+
+echo "Creating slug archive"
+tar -czf slug.tgz ./app
+
+echo "Creating slug object"
+_heroku_deploy_apikey="${HEROKU_API_KEY}" | base64
+_heroku_deploy_createSlugResponse=$(curl -x POST \
+-H "Content-Type: application/json" \
+-H "Accept: application/vnd.heroku+json; version=3" \
+-H "Authorization: ${_heroku_deploy_apiKey}" \
+-d '{"process_types":{ "web": "node-v0.10.20-linux-x64/bin/node web.js" }}' \
+-n https://api.heroku.com/apps/${HEROKU_APP_NAME_STAGE}/slugs)
+
+function _heroku_deploy_parseField {
+  echo -ne $2 | grep -o "\"$1\"\s*:\s*\"[^\"]*\"" | head -1 | cut -d '""' -f 4
+
+}
+_heroku_deploy_blobUrl=$(_heroku_deploy_parseField "url" "'${_heroku_deploy_createSlugResponse}'")
+_heroku_deploy_blobMethod=$(_heroku_deploy_parseField "method" "'${_heroku_deploy_createSlugResponse}'")
+_heroku_deploy_slugId=$(_heroku_deploy_parseField "id" "'${_heroku_deploy_createSlugResponse}'")
+
+echo "Uploading slug archive"
+curl -X ${_heroku_deploy_blobMethod^^} -H "Content-Type:" --data-binary @slug.tgz ${_heroku_deploy_blobUrl}
+
+function deployToHeroku { #Args: application name
+  curl -X POST \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/vnd.heroku+json; version=3" \
+  -H "Authorization: ${_heroku_deploy_apiKey}" \
+  -d "{\"slug\":\"${_heroku_deploy_slugId}\"}" \
+  -n https://api.heroku.com/apps/$1/releases
+}
+
+echo "Deploying slug to stage"
+deployToHeroku ${HEROKU_APP_NAME_STAGE}
